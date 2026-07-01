221257e7a088fc151c0c7d98ffb0e3af3d584202
Girma Moges <girmamoges244@gmail.com>
2026-07-01 10:20:17 +0300

    added stripe integration

--- config/appConfig.go
+++ config/appConfig.go
@@ -1,44 +1,37 @@
 package config
 
-import ( "errors"
-      "github.com/joho/godotenv"
-      "os"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
 )
 
-type AppConfig struct{
-	ServerPort string
-	Dsn string
-	AppSecret string
-	TwilioAccountSid string
-	TwilioAuthToken string
+type AppConfig struct {
+	ServerPort            string
+	Dsn                   string
+	AppSecret             string
+	TwilioAccountSid      string
+	TwilioAuthToken       string
 	TwilioFromPhoneNumber string
+	StripeSecret          string
+	PubKey                string
 }
 
-func SetupEnv() (cfg AppConfig, err error){
+func SetupEnv() (cfg AppConfig, err error) {
 
-//	if os.Getenv("APP_ENV") == "dev"{
+	if os.Getenv("APP_ENV") == "dev" {
 		godotenv.Load()
-	//}
+	}
 
-	httpPort := os.Getenv("HTTP_PORT")
-	Dsn:= os.Getenv("DSN")
+	httpPort := os.Getenv("SERVER_PORT")
+	Dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	appSecret := os.Getenv("APP_SECRET")
-	 
-	if len(httpPort) < 1{
-		return AppConfig{}, errors.New("env variable not found")
-	}
 
-	if len(Dsn)<1{
-		return AppConfig{}, errors.New("env variable not found")
-	}
-	if len(appSecret)<1{
-		return AppConfig{}, errors.New("app secret not found in the env variable")
-	}
-	 
-	return AppConfig{ServerPort: httpPort, Dsn:Dsn, AppSecret: appSecret,
-		TwilioAccountSid: os.Getenv("TWILIO_ACCOUNT_SID"),
-		TwilioAuthToken: os.Getenv("TWILIO_AUTH_TOKEN"),
+	return AppConfig{ServerPort: httpPort, Dsn: Dsn, AppSecret: appSecret,
+		TwilioAccountSid:      os.Getenv("TWILIO_ACCOUNT_SID"),
+		TwilioAuthToken:       os.Getenv("TWILIO_AUTH_TOKEN"),
 		TwilioFromPhoneNumber: os.Getenv("TWILIO_FROM_PHONE_NUMBER"),
-
-		},nil
+		StripeSecret:          os.Getenv("STRIPE_SECRET"),
+		PubKey:                os.Getenv("STRIPE_PUB_KEY"),
+	}, nil
 }
